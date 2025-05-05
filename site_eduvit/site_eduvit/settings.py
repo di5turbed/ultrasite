34e8dedb2e530be9d8eacce4b36745b11d54e86d
di5turbed <smirnofff.58@yandex.ru>
2025-05-05 17:40:44 +0300

    adaw

--- site_eduvit/site_eduvit/settings.py
+++ site_eduvit/site_eduvit/settings.py
@@ -107,7 +107,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
-STATICFILES_DIR = os.path.join(BASE_DIR, "media")
+STATICFILES_DIR = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
