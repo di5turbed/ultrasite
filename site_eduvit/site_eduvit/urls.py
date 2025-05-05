62778460346a5b007473c3e2d45c71ec38508b46
di5turbed <smirnofff.58@yandex.ru>
2025-05-05 17:20:05 +0300

    urls

--- site_eduvit/site_eduvit/urls.py
+++ site_eduvit/site_eduvit/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("mysite/", include("mysite.urls"), name="mysite"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
