351fa25b2f1d7082aad327e24a4b21d9e07ab1c6
di5turbed <smirnofff.58@yandex.ru>
2025-05-10 21:55:54 +0300

    Изменения в лучшую сторону(Крутые)

--- site_eduvit/mysite/models.py
+++ site_eduvit/mysite/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, verbose_name="Заголовок")
@@ -8,3 +9,10 @@ class BlogPost(models.Model):
     date_published = models.DateTimeField(default=timezone.now(), verbose_name="Дата публикации")
     image = models.ImageField(upload_to="images/", verbose_name="Изображение")
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Автор", null=True, blank=True)
+
+    def __str__(self):
+        return str(self.title)
+    
+    def get_absolute_url(self):
+        return reverse("blog:blog_detail", kwargs={"pk": self.pk})
+    
--- site_eduvit/site_eduvit/urls.py
+++ site_eduvit/site_eduvit/urls.py
@@ -6,8 +6,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("mysite/", include("mysite.urls"), name="mysite"),
+    path("mysite/", include("mysite.urls", namespace="mysite")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    
